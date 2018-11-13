33bb9d94e9ba64479d3a38435e8bb2e0c1ec56d3
saeed <saeed.samei85@gmail.com>
2018-11-14 00:25:40 +0330

    git initi

---
+++ lib_samei/src/main/java/com/example/lib_samei/MyClass.java
@@ -0,0 +1,13 @@
+package com.example.lib_samei;
+
+public class MyClass {
+     int a=10;
+     int b=20;
+     void printff(){
+
+        int sum;
+        sum=a+b;
+        System.out.println(sum);
+    }
+    }
+
---
+++ lib_samei/src/main/java/com/example/lib_samei/Subclass.java
@@ -0,0 +1,8 @@
+package com.example.lib_samei;
+
+public class Subclass extends MyClass {
+    public static void main(String[] args) {
+        Subclass a= new Subclass();
+        a.printff();
+    }
+}
